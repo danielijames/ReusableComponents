ff2431bf6a490edecb354aadf219cd461f8e5ae0
DanielJames <daniel.james@verizonconnect.com>
2020-05-14 10:12:54 -0400

    Initial Commit

---
+++ ReusuableComponents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusuableComponents
+//
+//  Created by Daniel James on 5/14/20.
+//  Copyright © 2020 Daniel James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
